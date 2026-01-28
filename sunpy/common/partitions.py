5057533c320507d2c9695bf871d63b02f503bafc
Samuel <samuel.gozel@epfl.ch>
2026-01-28 12:41:40 +0100

    Add License disclaimer at the top of each file

--- sunpy/common/partitions.py
+++ sunpy/common/partitions.py
@@ -1,17 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Copyright 2023 Samuel GOZEL, GNU GPLv3
+SUNPy A Python Library for solving SU(N) Heisenberg models
+Copyright (C) 2026  Samuel Gozel, GNU GPLv3
 
-Reference:
-https://www.ams.org/journals/mcom/1982-39-159/S0025-5718-1982-0658226-5/S0025-5718-1982-0658226-5.pdf
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-@author: sgozel
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import math
 
-
+# Reference:
+#   The Number of Partitions of the Integer N into M Nonzero Positive Integers
+#   W. J. A. Colman
+#   Mathematics of Computation, Vol. 39, Number 159, July 1982, Pages 213-224
+#   https://www.ams.org/journals/mcom/1982-39-159/S0025-5718-1982-0658226-5/S0025-5718-1982-0658226-5.pdf
 
 def p1(n):
     # number of partitions of n in 1 part
