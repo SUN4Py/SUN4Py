d07fbcbe846dc6fd3049e1fd473393e5979e0e78
Samuel <samuel.gozel@epfl.ch>
2026-02-04 22:55:18 +0100

    rmefund: remove old _atomic_run_old and __rme methods

--- sunpy/dmrg/rme/rmefund.py
+++ sunpy/dmrg/rme/rmefund.py
@@ -32,7 +32,8 @@ from sunpy.sdc.dmrg import sdcdmrg # shortcut method ("fast")
 
 class RMEEngineFund(RMEEngine):
     """
-    RME Engine for fundamental irrep at each site
+    Class to compute Reduced Matrix Elements (RMEs) of the Heisenberg interaction
+    for the fundamental irrep at each site
     """
     
     def __init__(self, N, num_irreps, target, tech, **kwargs):
@@ -45,21 +46,21 @@ class RMEEngineFund(RMEEngine):
             SU(N)
         num_irreps : int
             number of irreps
-        target : str
+        target : str ('GS')
             string describing the target irrep
         tech : str
             string describing the technique to use to compute RMEs ('base', 'shortcut_cols', 'shortcut_rows')
         
         [optional]
-        rme_folder : str [default]'path/to/SUNPy/sunpy/rme_coefficients/'
-            path to directory containing RME coefficients files
-        checkpointing : bool  [default]False
+        rme_folder : str [default: 'path/to/user/script/sunpy_rmes/']
+            path to directory containing files of RMEs
+        checkpointing : bool  [default: False]
             if True, perform checkpointing
-        chkpt_method : str [default]'log2', 'custom'
+        chkpt_method : str [default: 'log2'], 'custom'
             checkpointing strategy
         chkpt_ni : numpy array
             array of num_irreps to checkpoint
-        restarting : bool [default]True
+        restarting : bool [default: True]
             if True, attempt to restart from a saved file
         restarting_folder : str
             directory to search for restarting file
@@ -90,26 +91,23 @@ class RMEEngineFund(RMEEngine):
             used to create the "reduced" list with 24 irreps.
         """
         
-        assert(target in ['GS'])
-        
+        if not target in ['GS']:
+            raise ValueError('RMEEngineFund : __init__ : target sector undefined')
         if 'filename_prefix' in kwargs:
             self._filename_prefix = kwargs['filename_prefix']
         else:
             self._filename_prefix = 'RME_fund'
-        
         super().__init__(N, num_irreps, filename_prefix=self._filename_prefix, target=target, tech=tech, **kwargs)
-    
+        return
     
     def __str__(self):
         return f"RMEEngineFund, N={self._N}, num_irreps={self._num_irreps}, target={self._target}, tech={self._tech}"
-    
     
     def _init_irreps(self, num_irreps):
         """
         
         """
         return self._irreps_all[:num_irreps] + np.full(shape=(num_irreps, self._N), fill_value=1, dtype=int)
-    
     
     def _target_irrep(self, n):
         """
@@ -132,10 +130,9 @@ class RMEEngineFund(RMEEngine):
             ind = np.argwhere(np.sum(self._irreps_all, axis=1)==nr).flatten()
             alphaTarget += self._irreps_all[ind[0]]
         else:
-            sys.exit('RMEEngineFund.target_irrep: Target irrep undefined.')
+            raise ValueError('RMEEngineFund : _target_irrep: Target irrep undefined.')
         
         return alphaTarget
-    
     
     def _evaluate_compatibility(self, alpha1, alpha2):
         """
@@ -152,14 +149,10 @@ class RMEEngineFund(RMEEngine):
         
         return condition
     
-    
     def _get_sdc(self, alpha, alpha1, l1, alpha2, l2):
         """
         Compute SDCs for |alpha; alpha1, l1; alpha2, l2>
         """
-        
-        if not self._tech in ['base', 'shortcut_cols', 'shortcut_rows']:
-            sys.exit('tech undefined.')
         
         if self._tech=='base':
             
@@ -221,7 +214,6 @@ class RMEEngineFund(RMEEngine):
         
         return ydev, cydev, coeff
     
-    
     def _atomic_run(self):
         """
         Compute the reduced matrix elements
@@ -265,7 +257,7 @@ class RMEEngineFund(RMEEngine):
             condition_1 = self._evaluate_compatibility(alpha1, alpha2)
             
             if (mult_1>1):
-                sys.exit('Problem: mult_1>1')
+                sys.exit('ERROR : RMEEngineFund : _atomic_run : Problem: mult_1>1')
             elif ((mult_1==1) & condition_1):
                 
                 # construct ascendant shapes
@@ -300,7 +292,7 @@ class RMEEngineFund(RMEEngine):
                         condition_2 = self._evaluate_compatibility(alpha3, alpha4)
                         
                         if (mult_2>1):
-                            sys.exit('Problem: mult_2>1')
+                            sys.exit('ERROR : RMEEngineFund : _atomic_run : Problem: mult_2>1')
                         elif ((mult_2==1) & condition_2):
                             
                             doCalc = True
@@ -415,253 +407,3 @@ class RMEEngineFund(RMEEngine):
         self.save(self._filename)
         
         return
-    
-    
-    def _atomic_run_old(self, tech='base'):
-        """
-        Compute the reduced matrix elements
-        """
-        print('DEPRECATED _atomic_run_old This is an old, less efficient, method.')
-        
-        print('Start computing RME num_irreps = ', self.num_irreps)
-        ts = time.time()
-        
-        if self.restarting==True:
-            rme_reader_old = RMEReader(self.N, self.num_irreps_old, self.restarting_filename)
-        
-        states = states_rme(self.N, self.num_irreps, self.irreps)
-        num_states2 = states.shape[0]
-        
-        # compute reduced matrix elements
-    
-        liste_rme = []
-        indices_liste_rme = {}
-        index_liste_rme = int(0)
-        bool_indliste = np.full(shape=(num_states2,), fill_value=False, dtype=bool)
-        
-        for p in range(0, num_states2):
-            
-            cpt = int(0)        
-            Matp_ind = np.zeros(shape=(20,), dtype=int)
-            Matp_coeff = np.zeros(shape=(20,), dtype=float)
-            
-            alpha1 = self.irreps[states[p, 0]]
-            l1 = states[p, 1]
-            alpha2 = self.irreps[states[p, 2]]
-            l2 = states[p, 3]
-            
-            # build target irrep
-            alphaGS = self.target_irrep(np.sum(alpha1)+np.sum(alpha2))
-            
-            mult_1 = sun.multiplicity_irrep_mixed(alphaGS, np.array([alpha1, alpha2]), self.N)
-            
-            condition_1 = ( ((np.sum(alpha1)-np.sum(alpha2))%self.N==0) | ((np.sum(alpha1) + np.sum(alpha2))%self.N==0) )
-            
-            if ((mult_1==1) & condition_1):
-                
-                # construct ascendant shapes
-                alpha1_asc = np.copy(alpha1)
-                alpha1_asc[l1] -= 1
-                alpha2_asc = np.copy(alpha2)
-                alpha2_asc[l2] -= 1
-                
-                # get all descendants of the ascendants shapes
-                alpha1_desc, pos1_desc = sun.get_descendants(alpha1_asc, self.N)
-                alpha2_desc, pos2_desc = sun.get_descendants(alpha2_asc, self.N)
-                
-                nbf_1 = len(pos1_desc)
-                nbf_2 = len(pos2_desc)
-                
-                for qq in range(0, nbf_1):
-                    for pp in range(0, nbf_2):
-                        
-                        alpha3 = alpha1_desc[qq]
-                        l3 = pos1_desc[qq]
-                        
-                        alpha4 = alpha2_desc[pp]
-                        l4 = pos2_desc[pp]
-                        
-                        # ensure there is a column with N boxes
-                        alpha3p = alpha3 + (1-alpha3[-1])*np.full(shape=(self.N,), fill_value=1, dtype=int)
-                        alpha4p = alpha4 + (1-alpha4[-1])*np.full(shape=(self.N,), fill_value=1, dtype=int)
-                        
-                        mult_2 = sun.multiplicity_irrep_mixed(alphaGS, np.array([alpha3, alpha4]), self.N)
-                        
-                        condition_2 = ( ((np.sum(alpha3)-np.sum(alpha4))%self.N==0) | ((np.sum(alpha3) + np.sum(alpha4))%self.N==0) )
-                        
-                        if ((mult_2==1) & condition_2):
-                            
-                            doCalc = True
-                            
-                            if self.restarting:
-                                # search for indices in old list
-                                ind1_old = sunpy.common.math.find_row(self.irreps_old, alpha1)
-                                ind2_old = sunpy.common.math.find_row(self.irreps_old, alpha2)
-                                ind3_old = sunpy.common.math.find_row(self.irreps_old, alpha3p)
-                                ind4_old = sunpy.common.math.find_row(self.irreps_old, alpha4p)
-                                
-                                if len(ind1_old)*len(ind2_old)*len(ind3_old)*len(ind4_old)==1:
-                                    doCalc = False
-                            
-                            ind3 = sunpy.common.math.find_row(self.irreps, alpha3p)
-                            ind4 = sunpy.common.math.find_row(self.irreps, alpha4p)
-                            
-                            if ((len(ind3)>0) & (len(ind4)>0)):
-                                
-                                bra = np.array([ind3[0], l3, ind4[0], l4], dtype=int)
-                                index_bra = sunpy.common.math.find_row(states, bra)[0]
-                                
-                                if doCalc==True:
-                                    coeff = self.__rme(alphaGS, 
-                                                       alpha1, l1, 
-                                                       alpha2, l2, 
-                                                       alpha3, l3, 
-                                                       alpha4, l4, 
-                                                       tech)
-                                else:
-                                    coeff = rme_reader_old.read(alpha1, l1, 
-                                                                alpha2, l2, 
-                                                                alpha3p, l3, 
-                                                                alpha4p, l4)
-                                
-                                Matp_ind[cpt] = index_bra
-                                Matp_coeff[cpt] = coeff
-                                cpt += 1
-                        
-                        elif (mult_2>1):
-                            sys.exit('Problem: mult_2>1')
-            elif (mult_1>1):
-                sys.exit('Problem: mult_1>1')
-            
-            if cpt>0:
-                Matp_ind = Matp_ind[:cpt]
-                Matp_coeff = Matp_coeff[:cpt]
-                liste_rme.append([Matp_ind, Matp_coeff])
-                bool_indliste[p] = True
-                indices_liste_rme[p] = index_liste_rme
-                index_liste_rme += 1
-        
-        indliste = np.argwhere(bool_indliste==True).flatten()
-        
-        self.indliste = indliste
-        self.indices_liste_rme = indices_liste_rme
-        self.liste_rme = liste_rme
-        
-        te = time.time()
-        print('Done. Time = ', (te-ts)/60, 'min')
-        
-        self.save(self.filename)
-        
-        return
-    
-    
-    def __rme(self, alpha, alpha1, l1, alpha2, l2, alpha3, l3, alpha4, l4):
-        """
-        Reduced matrix element
-            <alpha3, l3; alpha4, l4 | P_{alpha} | alpha1, l1; alpha2, l2>
-        in SU(N)
-        
-        Parameters
-        ----------
-        self : RMEEngine
-            current instance
-        alpha : numpy array
-            global target irrep
-        alpha1 : numpy array
-            ket irrep, left block
-        l1 : int
-            position of last particle in ket irrep, left block
-        alpha2 : numpy array
-            ket irrep, right block
-        l2 : int
-            position of last particle in ket irrep, right block
-        alpha3 : numpy array
-            ket irrep, left block
-        l3 : int
-            position of last particle in bra irrep, left block
-        alpha4 : numpy array
-            ket irrep, right block
-        l4 : int
-            position of last particle in bra irrep, right block
-        tech : str
-            technique to use to compute the SDCs
-            'base', 'shortcut_cols', 'shortcut_rows'
-        
-        Returns
-        -------
-        out : float
-            reduced matrix element
-        
-        Details
-        -------
-        tech='base' : basic method for the SDCS, using the group co-chain (see Ref. [1])
-        tech='shortcut_cols' : shortcut in Chen's method, developing the irrep as a 
-                               product of its columns (see Ref. [2])
-        tech='shortcut_rows' : shortcut in Chen's method, developing the irrep as a
-                               product of its rows
-        
-        References
-        ----------
-        [1]         Group Representation Theory for Physicists
-                    Jin-Quan Chen, Hialun Ping and Fan Wang
-                    World Scientific, 2nd edition, (2002)
-        [2]         DMRG simulations of SU(N) Heisenberg chains using standard ...
-                    Pierre Nataf, Frederic Mila
-                    Phys. Rev. B 97, 134420 (2018)
-        """
-        
-        if not self._tech in ['base', 'shortcut_cols', 'shortcut_rows']:
-            sys.exit('tech undefined.')
-        
-        if self._tech=='base':
-            
-            ket_y, ket_cy, ket_coeff = sdcdmrgbase.get_SDC(self._N, alpha, 
-                                                    alpha1, l1, 
-                                                    alpha2, l2, 
-                                                    ref2firstLLOS=False, 
-                                                    ref1firstLLOS=True)
-            
-            bra_y, bra_cy, bra_coeff = sdcdmrgbase.get_SDC(self._N, alpha, 
-                                                    alpha3, l3, 
-                                                    alpha4, l4, 
-                                                    ref2firstLLOS=False, 
-                                                    ref1firstLLOS=True)
-        elif self._tech=='shortcut_cols':
-            
-            ket_y, ket_cy, ket_coeff = sdcdmrg.get_SDC(self._N, alpha, 
-                                                    alpha1, l1, 
-                                                    alpha2, l2, 
-                                                    ref2firstLLOS=False, 
-                                                    ref1firstLLOS=True)
-            
-            bra_y, bra_cy, bra_coeff = sdcdmrg.get_SDC(self._N, alpha, 
-                                                    alpha3, l3, 
-                                                    alpha4, l4, 
-                                                    ref2firstLLOS=False, 
-                                                    ref1firstLLOS=True)
-            
-        elif self._tech=='shortcut_rows':
-            
-            ket_y, ket_cy, ket_coeff = sdcdmrg.get_SDC(self._N, alpha, 
-                                                    alpha1, l1, 
-                                                    alpha2, l2, 
-                                                    ref2firstLLOS=True, 
-                                                    ref1firstLLOS=True)
-            
-            bra_y, bra_cy, bra_coeff = sdcdmrg.get_SDC(self._N, alpha, 
-                                                    alpha3, l3, 
-                                                    alpha4, l4, 
-                                                    ref2firstLLOS=True, 
-                                                    ref1firstLLOS=True)
-        
-        ket_coeff = ket_coeff.flatten()
-        bra_coeff = bra_coeff.flatten()
-        
-        n1 = np.sum(alpha1)
-        ket_y, ket_cy, ket_coeff = sun.develop_consecutive_number(alpha, ket_y, ket_cy, ket_coeff, n1-1)
-        
-        _, bra_ind, ket_ind = sunpy.common.math.intersect_row(bra_cy, ket_cy)
-        
-        out = np.sum( np.multiply(bra_coeff[bra_ind], ket_coeff[ket_ind]) )
-        
-        return out
